93fe1441b0f43cbf0c8e9da5643a59e93ce8dbd3
ShivamTaneja <shiivamtaneja@gmail.com>
2023-08-14 21:35:56 +0530

    add built-in midleware to parse JSON in req.body

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,6 +9,7 @@ const getContacts = (req, res) => {
 //@route POST /api/contacts
 //@access public
 const createContact = (req, res) => {
+  console.log(req.body);
   res.status(201).json({ message: "Create contacts" });
 };
 
